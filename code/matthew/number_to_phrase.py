e00eeca9dd6156147179413190b4b545b0dcca2e
Matthew J. Hughes <mjhughes707@gmail.com>
2019-07-11 21:20:26 -0700

    updated number_to_phrase.py

--- code/matthew/number_to_phrase.py
+++ code/matthew/number_to_phrase.py
@@ -1,7 +1,22 @@
 # filename: number_to_phrase.py
 
-# imports the conversion dictionaries
-from number_to_phrase_dicts import *
+# NUMBERS AS WORDS
+
+ones_table = {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine"}
+
+teens_table = {"1": "eleven", "2": "twelve", "3": "thirteen", "4": "fourteen", "5": "fifteen", "6": "sixteen", "7": "seventeen", "8": "eighteen", "9": "nineteen"}
+
+tens_table = {"1": "ten", "2": "twenty", "3": "thirty", "4": "forty", "5": "fifty", "6": "sixty", "7": "seventy", "8": "eighty", "9": "ninety"}
+
+hundreds_table = {"1": "one hundred", "2": "two hundred", "3": "three hundred", "4": "four hundred", "5": "five hundred", "6": "six hundred", "7": "seven hundred", "8": "eight hundred", "9": "nine hundred"}
+
+# NUMBERS AS ROMAN NUMERALS
+
+roman_ones_table = {"1": "I", "2": "II", "3": "III", "4": "IV", "5": "V", "6": "VI", "7": "VII", "8": "VIII", "9": "IX"}
+
+roman_tens_table = {"1": "X", "2": "XX", "3": "XXX", "4": "XL", "5": "L", "6": "LX", "7": "LXX", "8": "LXXX", "9": "XC"}
+
+roman_hundreds_table = {"1": "C", "2": "CC", "3": "CCC", "4": "CD", "5": "D", "6": "DC", "7": "DCC", "8": "DCCC", "9": "CM"}
 
 print("\nWelcome!\n")
 
