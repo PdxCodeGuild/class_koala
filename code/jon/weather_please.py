d0331cd4f70a535737036995c44ea938b313bead
Modesto Bascos <techmoby43@modestos-mbp.lan>
2019-07-27 10:36:41 -0700

    A little housekeeping

--- code/jon/weather_please.py
+++ code/jon/weather_please.py
@@ -10,55 +10,6 @@ import os
         - take that file and email it to me
 """
 
-# Gets latitude and longitude of the zipcode
-def get_latlon(zipcode):
-    map_api = 'VhicHGxyitNwdtTPZFczl8z9TeeM0voN'
-    map_url = 'http://www.mapquestapi.com/geocoding/v1/address?key=' + map_api + '&location=' + str(zipcode)
-
-    # Retrieves the info from the mapquest API
-    map_res = requests.get(map_url)
-    # Stores the retreived data as a JSON file
-    map_data = map_res.json()
-
-    # Extracts and stores the city name from the JSON file
-    map_location = map_data['results'][0]['locations'][0]['adminArea5']
-    # Extracts and stores the latitude of the zipcode from the JSON file
-    map_latitude = map_data['results'][0]['locations'][0]['latLng']['lat']
-    # Extracts and stores the longitude of the zipcode from the JSON file
-    map_longitude = map_data['results'][0]['locations'][0]['latLng']['lng']
-
-    return (map_latitude, map_longitude, map_location)
-
-def weather(zipcode):
-    api_key = "5f96bbce0e67b37965b9ef3af8b32ceb"
-    weather_latitude = get_latlon(req_zipcode)[0]
-    weather_longitude = get_latlon(req_zipcode)[1]
-    print(f'Latitude: {weather_latitude}')
-    print(f'Longitude: {weather_longitude}')
-    forecast = forecastio.load_forecast(api_key, weather_latitude, weather_longitude, units='us')
-    
-    # Stores the weather information from the darksky API as a JSON file
-    data = forecast.json
-
-    # Extracts and stores the current summary of the weather (sunny, cloudy, rainy, partly cloudy, etc.), current temperature, what temperature it actually feels like and the weather summary for the day
-    wanted_data = [data['currently']['summary'], data['currently']['temperature'], data['currently']['apparentTemperature'], data['daily']['summary']]
-    
-    return wanted_data
-
-def display(info):
-    # Gets the current time
-    now = datetime.now()
-    current_time = now.strftime('%H:%M')
-    weather = info[0]
-    current_temp = info[1]
-    feels_temp = info[2]
-    weather_summary = info[3]
-    city = get_latlon(req_zipcode)[2]
-
-    print(f'Currently at {current_time} in {city}, the weather is {weather}.')  
-    print(f'Your weather summary for the week is: {weather_summary}.')
-    print(f'The current temperature is {current_temp} degrees F but feels more like {feels_temp} degrees F.')
-
 def refresh_screen():
     os.system('clear')
 
@@ -78,20 +29,63 @@ def header():
     print('***********************************************************')
     print('***********************************************************\n')
 
+# Gets latitude and longitude of the zipcode
+def get_latlon(zipcode):
+    map_api = 'VhicHGxyitNwdtTPZFczl8z9TeeM0voN'
+    map_url = 'http://www.mapquestapi.com/geocoding/v1/address?key=' + map_api + '&location=' + str(zipcode)
+
+    # Retrieves the info from the mapquest API
+    map_res = requests.get(map_url)
+    # Stores the retreived data as a JSON file
+    map_data = map_res.json()
+
+    # Extracts and stores the city name from the JSON file
+    map_location = map_data['results'][0]['locations'][0]['adminArea5']
+    # Extracts and stores the latitude of the zipcode from the JSON file
+    map_latitude = map_data['results'][0]['locations'][0]['latLng']['lat']
+    # Extracts and stores the longitude of the zipcode from the JSON file
+    map_longitude = map_data['results'][0]['locations'][0]['latLng']['lng']
+
+    return (map_latitude, map_longitude, map_location)
+
+# Gets the weather from the Dark Sky API using the Lat/Lon from get_latlon()
+def weather(zipcode):
+    api_key = "5f96bbce0e67b37965b9ef3af8b32ceb"
+    weather_latitude = get_latlon(req_zipcode)[0]
+    weather_longitude = get_latlon(req_zipcode)[1]
+    print(f'Latitude: {weather_latitude}')
+    print(f'Longitude: {weather_longitude}')
+    forecast = forecastio.load_forecast(api_key, weather_latitude, weather_longitude, units='us')
+    
+    # Stores the weather information from the darksky API as a JSON file
+    data = forecast.json
+
+    # Extracts and stores the current summary of the weather (sunny, cloudy, rainy, partly cloudy, etc.), current temperature, what temperature it actually feels like and the weather summary for the day
+    wanted_data = [data['currently']['summary'], data['currently']['temperature'], data['currently']['apparentTemperature'], data['daily']['summary']]
+    
+    return wanted_data
+
+# Displays all of the info 
+def display(info):
+    # Gets the current time
+    now = datetime.now()
+    current_time = now.strftime('%H:%M')
+    weather = info[0]
+    current_temp = info[1]
+    feels_temp = info[2]
+    weather_summary = info[3]
+    city = get_latlon(req_zipcode)[2]
+
+    print(f'Currently at {current_time} in {city}, the weather is {weather}.')  
+    print(f'Your weather summary for the week is: {weather_summary}.')
+    print(f'The current temperature is {current_temp} degrees F but feels more like {feels_temp} degrees F.')
+
+# Main
 
 name = input('What is your first name? ')
 refresh_screen()
 print (f'\nWelcome, {name}, to........\n')
 header()
-
 req_zipcode = input(f'What zipcode do you want to get the weather for? ')
 
-# print(weather(97045))
-# print(display(weather(97045)))
 display(weather(req_zipcode))
-
-
-# print(f'Your lan/lon is: {get_lanlon(97045)}')
-# print(type(get_lanlon(97045)))
-# print(f'Your Latitude is: {get_lanlon(97045)[0]}')
-# print(f'Your Longitude is: {get_lanlon(97045)[1]}')
