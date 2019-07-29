3a0ae669e532db36f65b795e1116600f6555b2dd
Matthew J. Hughes <mjhughes707@gmail.com>
2019-07-28 21:46:10 -0700

    updated poliplotter.py - organized code into functions

--- code/matthew/poliplotter.py
+++ code/matthew/poliplotter.py
@@ -11,54 +11,78 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator # used to genera
 # ***** AUTHENTICATION *****
 # ***** HIDDEN FOR SECURITY *****
 
-try:
-    # Creates the authentication object
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    # Sets access token and secret
-    auth.set_access_token(access_token, access_token_secret)
-    # creates tweepy API object to fetch tweets
-    api = tweepy.API(auth)
-except:
-    print("\nError: Authentication Failed\n")
-    quit()
+def authenicator():
+    """creates the authenication object, sets access token and secret, then creates the tweepy API object to fetch tweets"""
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+    except:
+        print("\nError: Authentication Failed\n")
+        quit()
+    return api
 
-name = input("\nWithout including the @ symbol,\nPlease type the username of the requested account: ") # specifies target user
-tweetCount = int(input("How many tweets would you like to analyze? ")) # number of tweets to pull
+def target_user():
+    name = input("\nWithout including the @ symbol,\nPlease type the username of the requested account: ") # specifies target user
+    tweetCount = int(input("How many tweets would you like to analyze? ")) # number of tweets to pull
+    mask = np.array(Image.open("img/trump_portrait.jpg")) # opens the image file, converts to a NumPy array and stores as the "mask" variable
+    return name, tweetCount, mask
 
-# calls the user_timeline function with our parameters
-results = api.user_timeline(id=name, count=tweetCount, tweet_mode="extended")
+def results(api, name, tweetCount):
+    """gathers x number of tweets from the user and returns the results"""
+    results = api.user_timeline(id=name, count=tweetCount, tweet_mode="extended")
+    return results
 
-# cycles through full results, checks against RTs and then adds the rest to one complete string
-tweet_string = ""
-for tweet in results:
-    full_tweet = tweet.full_text
-    if (not tweet.retweeted) and ('RT @' not in tweet.full_text):
-        tweet_string += full_tweet
+def compiler(results):
+    """cycles through full results, checks against RTs and then adds the rest to one complete string"""
+    tweet_string = ""
+    for tweet in results:
+        full_tweet = tweet.full_text
+        if (not tweet.retweeted) and ('RT @' not in tweet.full_text):
+            tweet_string += full_tweet
+    return tweet_string
 
 def clean_tweet(tweets):
     """Uses regex statements to clean tweets by removing links and special characters"""
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", tweets).split())
 
+def string_prep(clean_tweets):
+    """prepares the tweets for later use by the wordcloud function"""
+    tweet_words = TextBlob(clean_tweets) # allows for tweets to be manipulated
+    words = tweet_words.words # creates a WordList
+    word_string = " ".join(words) # joins as one string
+    return word_string
 
-clean_tweets = clean_tweet(tweet_string) # cleans tweets
-tweet_words = TextBlob(clean_tweets)
-words = tweet_words.words # separates into individual words
-word_string = " ".join(words) # joins as one string
+def stopwords():
+    """establishes stopwords and adds on some custom ones"""
+    stopwords = set(STOPWORDS)
+    stopwords.update(["amp", "will", "need", "much", "us", "Im", "cant", "going", "thats"])
+    return stopwords
 
-# Generate a word cloud image
-mask = np.array(Image.open("img/trump_portrait.jpg"))
+def wordcloud(word_string, stopwords):
+    """creates the wordcloud object"""
+    try:
+        wc = WordCloud(stopwords=stopwords, background_color="white", max_words=10000, mask=mask).generate(word_string)
+        return wc
+    except ValueError:
+        print(f"We're sorry. '{name}' is not a valid Twitter user.\n")
+        quit()
 
-# establishes stopwords and adds a few custom ones
-stopwords = set(STOPWORDS)
-stopwords.update(["amp", "will", "need", "much", "us", "Im", "cant", "going", "thats"])
+def show(mask, wc):
+    """displays the generated image"""
+    image_colors = ImageColorGenerator(mask) # create color from image
+    plt.figure(figsize=[16,9])
+    plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
 
-"""creates the wordcloud object"""
-wc = WordCloud(stopwords=stopwords, background_color="white", max_words=10000, mask=mask).generate(word_string)
-
-# displays the generated image
-# create coloring from image
-image_colors = ImageColorGenerator(mask)
-plt.figure(figsize=[16,9])
-plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
-plt.axis("off")
-plt.show()
+if __name__ == '__main__':
+    api = authenicator()
+    name, tweetCount, mask = target_user()
+    results = results(api, name, tweetCount)
+    tweet_string = compiler(results)
+    clean_tweets = clean_tweet(tweet_string)
+    word_string = string_prep(clean_tweets)
+    stopwords = stopwords()
+    wc = wordcloud(word_string, stopwords)
+    show(mask, wc)
