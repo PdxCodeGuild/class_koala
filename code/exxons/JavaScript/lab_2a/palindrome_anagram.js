f559c0e6ce1b3b8572c5fa69eccff01a78dc5411
exxon <suarez.exxon@gmail.com>
2019-08-12 16:32:25 -0700

    updated

---
+++ code/exxons/JavaScript/lab_2a/palindrome_anagram.js
@@ -0,0 +1,22 @@
+
+
+let user_word = prompt("which word would you like to check?");
+
+
+let reversed_word_list = [];
+let ordered_word_list = [];
+
+function check_anagram (user_word) {
+    user_word.toLowerCase()
+    
+    no_space_string = user_word.replace(/\s/g, '');
+
+    non_arranged_word = (no_space_string)
+
+    
+
+
+
+
+
+}
