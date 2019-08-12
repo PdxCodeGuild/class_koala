3b1686d9ce2fe20fac0789bc81fd29cc31c7593b
exxon <suarez.exxon@gmail.com>
2019-08-12 15:54:17 -0700

    added lab1a

--- code/exxons/JavaScript/lab_1a/unit_converter.js
+++ code/exxons/JavaScript/lab_1a/unit_converter.js
@@ -1,9 +1,8 @@
-let user_feet = prompt(' Please input the number of feet you would like\
-converted into meters: ');
+let user_feet = prompt(' Please input the number of feet you would like converted into meters: ');
 
-let respnse = user_feet * 0.3048
+let response = user_feet * 0.3048
 
-alert(user_feet)
+alert (response)
 
 
 
