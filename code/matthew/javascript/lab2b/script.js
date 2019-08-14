11b10709765923749d7bbe3260b5b523e85cf5a2
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-14 16:27:37 -0700

    changed card fields to dropdowns - need to update JS to match

--- code/matthew/javascript/lab2b/script.js
+++ code/matthew/javascript/lab2b/script.js
@@ -6,15 +6,11 @@ let card5 = document.getElementById("card5");
 let evaluate = document.getElementById("evaluate");
 let advice = document.getElementById("advice");
 
-let card_values = {
-  "K": 10, "Q": 10, "J": 10, "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "A": 1
-}
-
 function bj_advice() {
   let card_list = [];
-  let card_value = card_values[(card1.value).toUpperCase()];
+  let card_value = card1.value;
   card_list.push(card_value);
-  card_value = card_values[(card2.value).toUpperCase()];
+  card_value = card2.value;
   card_list.push(card_value);
   card_value = card_values[(card3.value).toUpperCase()];
   if (card_value === undefined) {
