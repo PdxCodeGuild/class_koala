cd979073a6f806eec84e3103aee5d5f441850fd0
Jon Bascos <techmoby43@Jons-MacBook-Pro.local>
2019-08-14 11:33:36 -0700

    finished creating cipher site.  Considering adding background image or even just images

---
+++ code/jon/JavaScript/Lab 1b/main.js
@@ -0,0 +1,23 @@
+let originalWord = document.getElementById('originalWord');
+let button = document.getElementById('button');
+let cipherAnswer = document.getElementById('cipher');
+
+
+function cipher() {
+    let word = originalWord.value;
+    console.log(`word is ${word}`);
+    let letters = 'abcdefghijklmnopqrstuvwxyz';
+    let cipherLetters = 'nopqrstuvwxyzabcdefghijklm';
+    let encrypted = '';
+
+    for (let i = 0; i < word.length; i++) {
+        letterIndex = letters.indexOf(word[i]);
+        encrypted += cipherLetters.substr(letterIndex, 1);
+    };
+    
+    cipherAnswer.innerText = encrypted;
+}
+
+button.addEventListener('click', cipher);
+
+
