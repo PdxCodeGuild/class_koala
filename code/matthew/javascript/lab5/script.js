c80b2a909ee7a6da23ef336d6e5da77d34a3f44c
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-17 18:11:33 -0700

    completed Lab5 - Version 2

--- code/matthew/javascript/lab5/script.js
+++ code/matthew/javascript/lab5/script.js
@@ -7,11 +7,23 @@ socials = [
   "github.com/mjhughesio"
 ]
 
-randomSite = function() {
+function randomSite() {
   let randomIndex = Math.random() * socials.length;
   randomIndex = parseInt(randomIndex, 10);
   let link = "https://" + socials[randomIndex];
   window.open(link);
 };
 
-randomSite();
+window.onload = function() {
+  let counter = 5;
+  setInterval(function() {
+    counter--;
+    if (counter >= 0) {
+      countdown = document.getElementById("countdown");
+      countdown.innerText = counter;
+    }
+    if (counter === 0) {
+      randomSite();
+    }
+  }, 1000)
+};
