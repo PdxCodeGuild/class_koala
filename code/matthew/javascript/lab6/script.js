b4035118cc5545774502bf329adac3b0650f214b
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-19 12:30:39 -0700

    created HTML & CSS / began JS

---
+++ code/matthew/javascript/lab6/script.js
@@ -0,0 +1,7 @@
+var d = new Date();
+var h = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+document.getElementById("hours").innerText = h + ":";
+document.getElementById("minutes").innerText = m + ":";
+document.getElementById("seconds").innerText = s;
