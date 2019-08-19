fa0c818874f92475d00bc5474f835c58efb5c2d7
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-19 13:53:22 -0700

    completed Lab6 - Version 1

--- code/matthew/javascript/lab6/script.js
+++ code/matthew/javascript/lab6/script.js
@@ -1,7 +1,10 @@
-var d = new Date();
-var h = d.getHours();
-var m = d.getMinutes();
-var s = d.getSeconds();
-document.getElementById("hours").innerText = h + ":";
-document.getElementById("minutes").innerText = m + ":";
-document.getElementById("seconds").innerText = s;
+setInterval (function() {
+  let d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+  m = ( m < 10 ? "0" : "" ) + m;
+  s = ( s < 10 ? "0" : "" ) + s;
+  let t = h + ":" + m + ":" + s;
+  document.getElementById("clock-time").innerText = t;
+}, 1000);
