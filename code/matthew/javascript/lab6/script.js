a922b928b0936f88328c00d11bd6b1bd11327a8d
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-19 16:29:12 -0700

    added 'Stop' and 'Reset' feature

--- code/matthew/javascript/lab6/script.js
+++ code/matthew/javascript/lab6/script.js
@@ -5,6 +5,7 @@ setInterval (function() {
   let h = d.getHours();
   let m = d.getMinutes();
   let s = d.getSeconds();
+  h = (h < 10 ? "0" : "") + h;
   m = (m < 10 ? "0" : "") + m;
   s = (s < 10 ? "0" : "") + s;
   let t = h + ":" + m + ":" + s;
@@ -13,10 +14,11 @@ setInterval (function() {
 
 // STOPWATCH - START //
 
+let stopTimer; // related to STOP function
 function stopwatch() {
   let d = new Date();
   d.setHours(0, 0, 0, 0);
-  setInterval (function() {
+  stopTimer = setInterval (function() {
     d.setSeconds(d.getSeconds() + 1);
     let h = d.getHours();
     let m = d.getMinutes();
@@ -33,3 +35,21 @@ var start = document.getElementById("start");
 start.addEventListener("click", stopwatch);
 
 // STOPWATCH - STOP //
+
+var stop = document.getElementById("stop");
+stop.addEventListener("click", function() {
+  clearInterval(stopTimer)
+});
+
+// STOPWATCH - LAP //
+
+
+
+
+// STOPWATCH - RESET //
+
+var reset = document.getElementById("reset");
+reset.addEventListener("click", function() {
+  clearInterval(stopTimer);
+  document.getElementById("stopwatch-time").innerText = "00:00:00";
+});
