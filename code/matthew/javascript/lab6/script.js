62cc42aa0eef7baaffdec26a4735635d45b794c8
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-20 11:26:57 -0700

    completed 'Countdown' / need to add toggle and animation

--- code/matthew/javascript/lab6/script.js
+++ code/matthew/javascript/lab6/script.js
@@ -67,3 +67,32 @@ reset.addEventListener("click", function() {
 
 document.getElementById("stopwatch-time").innerText = "00:00:00"; // sets default on load
 
+// COUNTDOWN
+
+function countdown() {
+  // stores all of the user values
+  let cd = document.getElementById("countdown-date").value;
+  let ch = document.getElementById("countdown-hours").value;
+  let cm = document.getElementById("countdown-minutes").value;
+  let cs = document.getElementById("countdown-seconds").value;
+  let countdownDate = new Date(`${cd} ${ch}:${cm}:${cs}`).getTime(); // compiles together
+  let counter = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countdownDate - now;
+    // math to determine days, etc. based on seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown-time").innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    // sets what occurs when countdown reaches zero
+    if (distance <= 1) {
+      clearInterval(counter);
+      document.getElementById("countdown-time").innerText = "0d 0h 0m 0s";
+      document.getElementById("countdown-complete").style.backgroundImage = "url('balloons.jpg')";
+    }
+  }, 1000);
+}
+
+let countdownStart = document.getElementById("countdown-start");
+countdownStart.addEventListener("click", countdown);
