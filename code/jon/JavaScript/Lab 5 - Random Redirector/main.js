e1d564c66b92c083c3b16da6c2882d5153bee34a
jonbascos <jon@jonbascos.com>
2019-08-19 22:57:21 -0700

    working on lab 5 version 2

--- code/jon/JavaScript/Lab 5 - Random Redirector/main.js
+++ code/jon/JavaScript/Lab 5 - Random Redirector/main.js
@@ -1,3 +1,4 @@
+function randomSites () {
 let random_urls = [
     'https://www.google.com',
     'https://www.twitter.com',
@@ -10,3 +11,23 @@ let max = random_urls.length;
 randomNumber = Math.floor(Math.random() * (max - min + 1));
 
  location = random_urls[randomNumber];
+};
+//Version 2 JS
+
+//Display Countdown Timer
+
+let timer = setTimeout(myTimer, 5000)
+
+function myTimer () {
+    let seconds = document.getElementById('countdown').textContent;
+    let countdown = setInterval(function() {
+        seconds--;
+        if (seconds <=0) clearInterval(countdown) {
+            randomSites();
+        }
+    })
+
+};
+
+//After 5 seconds, redirects to a random URL listed above
+
