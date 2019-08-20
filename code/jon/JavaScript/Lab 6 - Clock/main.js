ee0103c7f6c41a95a95ddfd73b120cacd379a4b4
jonbascos <jon@jonbascos.com>
2019-08-20 16:30:39 -0700

    starting on the stopwatch of lab 6

--- code/jon/JavaScript/Lab 6 - Clock/main.js
+++ code/jon/JavaScript/Lab 6 - Clock/main.js
@@ -2,13 +2,65 @@
 
 
 
-let currentTime = setInterval(function() {
+//Clock
 
-    let date = new Date();
+// let currentTime = setInterval(function() {
 
-    let clockPlacement = document.getElementById('clock');
+//     let date = new Date();
 
-    clockPlacement.innerText = `The Current Time is: \n ${date.toLocaleTimeString()}`;
+//     let clockPlacement = document.getElementById('clock');
 
-}, 1000)
+//     clockPlacement.innerText = `The Current Time is: \n ${date.toLocaleTimeString()}`;
+
+// }, 1000)
+
+
+//Stopwatch
+
+
+function stopwatch() {
+    //create the stopwatch
+
+    //initialize the timer
+    let initTimer = new Date();
+    initTimer.setHours(0,0,0,0);
+    
+
+    //Get the Hours, Minutes, Seconds
+    let hours = initTimer.getHours();
+    let minutes = initTimer.getMinutes();
+    let seconds = initTimer.getSeconds();
+    let milliseconds = initTimer.getMilliseconds();
+
+    //Adds the extra zero in the tens place if the number is less than 10
+    hours = (hours < 10 ? "0" : '') + hours;
+    minutes = (minutes < 10 ? "0" : '') + minutes;
+    seconds = (seconds < 10 ? "0" : '') + seconds;
+
+    //start counting and displaying the new time every second
+
+    startWatch = setInterval(function() {
+        //Display the watch
+        let stopwatchLocation = document.getElementById('actual-stopwatch');
+        let stopwatchDisplay = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+        stopwatchLocation.innerText = stopwatchDisplay;
+        
+    }, 1000)
+};
+
+let startButton = document.getElementById('startButton');
+let lapButton = document.getElementById('lapButton');
+
+startButton.addEventListener('click', stopwatch);
+
+// let stopwatchDate = new Date();
+// stopwatchDate.setHours(0,0,0,0);
+// let stopwatchLocation = document.getElementById('actual-stopwatch');
+
+// let minutes = stopwatchDate.getMinutes();
+// let seconds = stopwatchDate.getSeconds();
+// let ms = stopwatchDate.getMilliseconds();
+
+// stopwatchLocation.innerText = `${minutes}:${seconds}:${ms}`;
+
 
