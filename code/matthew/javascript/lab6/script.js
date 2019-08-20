d97730a8bb125f26a5e60a3e2e7309a511520d07
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-20 14:28:18 -0700

    added toggle feature

--- code/matthew/javascript/lab6/script.js
+++ code/matthew/javascript/lab6/script.js
@@ -96,3 +96,57 @@ function countdown() {
 
 let countdownStart = document.getElementById("countdown-start");
 countdownStart.addEventListener("click", countdown);
+
+// TOGGLE
+
+function toggle(e) {
+  console.log(e);
+  clock = document.getElementById("clock-header");
+  stopwatch = document.getElementById("stopwatch-header");
+  countdown = document.getElementById("countdown-header");
+  let main = document.getElementById("main");
+  let clockMain = document.getElementById("clock-main");
+  let clockImage = document.getElementById("clock-image");
+  let stopwatchMain = document.getElementById("stopwatch-main");
+  let stopwatchLaps = document.getElementById("stopwatch-laps");
+  let countdownMain = document.getElementById("countdown-main");
+  let countdownComplete = document.getElementById("countdown-complete");
+  if (e.target == clock) {
+    main.style.gridTemplateAreas = "'countdown clock stopwatch'";
+    clockMain.style.display = "flex";
+    clockImage.style.display = "flex";
+    clockMain.style.borderTop = "1px solid black";
+    clockMain.style.borderBottom = "1px solid black";
+    countdownMain.style.display = "none";
+    countdownComplete.style.display = "none";
+    stopwatchMain.style.display = "none";
+    stopwatchLaps.style.display = "none";
+  }
+  if (e.target == countdown) {
+    countdownMain.style.display = "flex";
+    countdownComplete.style.display = "flex";
+    countdownMain.style.borderTop = "1px solid black";
+    countdownMain.style.borderBottom = "1px solid black";
+    main.style.gridTemplateAreas = "'stopwatch countdown clock'";
+    stopwatchMain.style.display = "none";
+    stopwatchLaps.style.display = "none";
+    clockMain.style.display = "none";
+    clockImage.style.display = "none";
+  }
+  if (e.target == stopwatch) {
+    stopwatchMain.style.display = "flex";
+    stopwatchLaps.style.display = "flex";
+    stopwatchMain.style.borderTop = "1px solid black";
+    stopwatchMain.style.borderBottom = "1px solid black";
+    main.style.gridTemplateAreas = "'clock stopwatch countdown'";
+    clockMain.style.display = "none";
+    clockImage.style.display = "none";
+    countdownMain.style.display = "none";
+    countdownComplete.style.display = "none";
+  }
+}
+
+let h1s = document.querySelectorAll("h1");
+h1s.forEach(function(h1) {
+  h1.addEventListener("click", toggle);
+});
