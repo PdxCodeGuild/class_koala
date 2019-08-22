39ab9aa4e78c2265945e35d189485efd26e3e9a4
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-22 16:15:06 -0700

    almost completed Lab 8 - just need to add gravity

--- code/matthew/javascript/lab8/script.js
+++ code/matthew/javascript/lab8/script.js
@@ -1,22 +1,11 @@
-// sets initial variables
-let cnv = document.getElementById("cnv");
-let ctx = cnv.getContext("2d");
-
-// sets canvas size and background
-let width = 1400;
-let height = 760;
-ctx.fillStyle = "rgba(255, 255, 255, 0.25)";
-ctx.fillRect(0, 0, width, height);
-
 function drawBall() {
-
-  // DRAWS BALL CIRCLE AND FILLS IT WHITE-ISH
+  // DRAWS BALL CIRCLE AND FILLS IT WHITE-ISH //
   ctx.beginPath();
   ctx.arc(ball.px, ball.py, ball.radius, 0, 2 * Math.PI, false);
   ctx.fillStyle = "#f9f9f9";
   ctx.fill();
 
-  // DRAWS OUTER SHADOW
+  // DRAWS OUTER SHADOW //
   ctx.beginPath();
   ctx.arc(ball.px, ball.py, ball.radius, 0, 2 * Math.PI, false);
   ctx.strokeStyle = "rgba(0, 0, 0, .5)";
@@ -55,7 +44,7 @@ function drawBall() {
   ctx.lineTo(ball.px, ball.py - 31.25);
   ctx.stroke();
 
-  // DRAWS LINES FROM EACH CORNER OF PENTAGON
+  // DRAWS LINES FROM EACH CORNER OF PENTAGON // 
   // top line
   ctx.beginPath();
   ctx.moveTo(ball.px, ball.py - 31.25);
@@ -86,7 +75,7 @@ function drawBall() {
   ctx.lineTo(ball.px + 71, ball.py - 31);
   ctx.stroke();
 
-  // DRAWS OUTER PENTAGONS
+  // DRAWS OUTER PENTAGONS //
   // top two lines
   ctx.beginPath();
   ctx.moveTo(ball.px, ball.py - 75);
@@ -131,23 +120,69 @@ function drawBall() {
   ctx.moveTo(ball.px + 50, ball.py + 60);
   ctx.lineTo(ball.px + 42, ball.py + 90);
   ctx.stroke(); // right
-
 }
+
+// NEGATES VELOCITY AND INCREASES FRICTION //
+function flipAndFricX() { // left and right borders
+  ball.vx *= -1;
+  ball.vx *= friction;
+  ball.vy *= friction;
+}
+function flipAndFricY () { // top and bottom borders
+  ball.vy *= -1;
+  ball.vx *= friction;
+  ball.vy *= friction;
+}
+
+function moveBall() {
+  ball.px += ball.vx; // horizontal speed based on velocity
+  ball.py += ball.vy; // vertical speed based on velocity
+  // left border
+  if (ball.px - ball.radius < 0) {
+    ball.px = ball.radius + movement;
+    flipAndFricX();
+  // right border
+  } else if (ball.px + ball.radius > width) {
+      ball.px = width - ball.radius - movement;
+      flipAndFricX();
+  }
+  // top border
+  if (ball.py - ball.radius < 0) {
+    ball.py = ball.radius + movement;
+    flipAndFricY();
+  // bottom border
+  } else if (ball.py + ball.radius > height) {
+      ball.py = height - ball.radius - movement;
+      flipAndFricY();
+  }
+}
+
+function loop() {
+  ctx.clearRect(0, 0, width, height); // eliminates "image drag"
+  moveBall();
+  drawBall();
+  window.requestAnimationFrame(loop);
+}
+
+// sets initial variables
+let cnv = document.getElementById("cnv");
+let ctx = cnv.getContext("2d");
+
+// sets canvas size
+let width = 1400;
+let height = 760;
 
 // sets ball size, location and velocity
 let ball = {
   radius: 100,
   px: Math.random() * width, // ball's x location
   py: Math.random() * height, // ball's y location
-  vx: (2 * Math.random() - 1) * 10, // ball's x velocity
-  vy: (2 * Math.random() - 1) * 10 // ball's y velocity
+  vx: 12, // ball's x velocity
+  vy: 12 // ball's y velocity
 };
 
-function main_loop() {
-  px = px + 1;
-  py = py + 1;
-  drawBall();
-  window.requestAnimationFrame(main_loop);
-}
+// sets loop variables
+let friction = .99; // slows down ball after each collision with image border
+let movement = .001; // moves x or y positions
 
-window.requestAnimationFrame(main_loop);
+window.requestAnimationFrame(loop); // enters loop
