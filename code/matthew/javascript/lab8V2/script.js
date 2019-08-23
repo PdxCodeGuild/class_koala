613b742d3654db4d985f398119440742ae53df78
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-23 16:28:22 -0700

    working on 2nd version of Lab 8 - need to change colors each time it hits border

---
+++ code/matthew/javascript/lab8V2/script.js
@@ -0,0 +1,89 @@
+/********** FUNCTIONS **********/
+
+function drawTV() {
+  // draws the television
+  ctx.beginPath();
+  ctx.fillStyle = "rgb(33, 33, 33)"; // inner TV
+  ctx.rect(0, 0, width, height);
+  ctx.fill();
+  ctx.strokeStyle = "rgb(20, 20, 20)"; // outer border
+  ctx.lineWidth = tvBorder;
+  ctx.stroke();
+}
+
+function drawCube() {
+  ctx.fillStyle = cube.colors[0];
+  ctx.fillRect(cube.px - cube.radius/2, cube.py - cube.radius/2, cube.radius, cube.radius);
+}
+
+function addFont() {
+  // adds the DVD font
+  ctx.fillStyle = "black";
+  ctx.font = "50px Impact";
+  let topText = "DVD";
+  let topTextWidth = ctx.measureText(topText).width;
+  ctx.fillText(topText, cube.px - topTextWidth/2, cube.py);
+  // adds the video font
+  ctx.fillStyle = "white";
+  ctx.font = "29px Impact";
+  let bottomText = "VIDEO";
+  let bottomTextWidth = ctx.measureText(bottomText).width;
+  ctx.fillText(bottomText, cube.px - bottomTextWidth/2, cube.py + (cube.radius/4));
+}
+
+function moveCube() {
+  cube.px += cube.vx; // horizontal speed based on velocity
+  cube.py += cube.vy; // vertical speed based on velocity
+  // left border
+  if (cube.px - cube.radius/2 < tvBorder/2) {
+    cube.px = tvBorder/2 + cube.radius/2;
+    cube.vx *= -1;
+  // right border
+  } else if (cube.px + cube.radius/2 > (width - tvBorder/2)) {
+    cube.px = (width - tvBorder/2) - cube.radius/2;
+    cube.vx *= -1;
+  }
+  // top border
+  if (cube.py - cube.radius/2 < tvBorder/2) {
+    cube.py = tvBorder/2 + cube.radius/2;
+    cube.vy *= -1;
+  // bottom border
+  } else if (cube.py + cube.radius/2 > (height - tvBorder/2)) {
+    cube.py = (height - tvBorder/2) - cube.radius/2;
+    cube.vy *= -1;
+  }
+}
+
+function mainLoop() {
+  window.requestAnimationFrame(mainLoop);
+  ctx.clearRect(0, 0, width, height); // eliminates "image drag"
+  drawTV();
+  moveCube();
+  drawCube();
+  addFont();
+}
+
+/***********************************************/
+
+
+// sets initial variables
+let cnv = document.getElementById("cnv");
+let ctx = cnv.getContext("2d");
+
+// sets canvas size variables for use in functions
+// (800:600 ratio + outside border)
+let width = 960;
+let height = 760;
+let tvBorder = 80;
+
+// sets cube size, random position, velocity and movement
+let cube = {
+  colors: ["red", "limegreen", "blue"],
+  radius: 130,
+  px: Math.random() * width, // cube's x position
+  py: Math.random() * height, // cube's y position
+  vx: 3, // cube's x velocity
+  vy: 3, // cube's y velocity
+};
+
+window.requestAnimationFrame(mainLoop); // enters loop
