a8656e01e55949003c460ecc25c3ee5a593a8929
exxon <suarez.exxon@gmail.com>
2019-08-30 16:28:12 -0700

    updated

---
+++ code/exxons/JavaScript/lab_7/hacker_typer.js
@@ -0,0 +1,17 @@
+
+let hackerText = document.getElementById("text");
+let i = 0;
+
+
+
+document.body.addEventListener("keydown", function() {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+});
+
+
