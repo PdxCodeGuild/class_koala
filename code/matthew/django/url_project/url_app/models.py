b47b978dec9113a9ed855549b8db3b299a776ff3
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-04 15:58:19 -0700

    added a stats lookup feature

--- code/matthew/django/url_project/url_app/models.py
+++ code/matthew/django/url_project/url_app/models.py
@@ -13,3 +13,6 @@ class ClickData(models.Model):
     ip = models.CharField(max_length=45)
     time = models.DateTimeField(default = timezone.now)
     url = models.ForeignKey(UrlShortener, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ip
--- code/matthew/django/url_project/url_app/urls.py
+++ code/matthew/django/url_project/url_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"), # /url_app/
     path("submit/", views.submit, name="submit"), # /url_app/submit/
     path("code/<short_url>", views.redirect, name="redirect"), # url_app/code/PMTJg9
+    path("stats/<short_url>", views.stats, name="stats"), # url_app/stats/PMTJg9
 ]
 
