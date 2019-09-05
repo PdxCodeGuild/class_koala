221230efe7f5370e60a3dba3e55ac3d9bb216bf1
Cole Nation <52674619+Nation36@users.noreply.github.com>
2019-09-05 12:28:13 -0700

    .

--- code/cole/Django/blog_project/posts/urls.py
+++ code/cole/Django/blog_project/posts/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name = 'home')
+    path('', views.BlogListView.as_view(), name = 'home'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name = 'detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name = 'new'),
 ]
