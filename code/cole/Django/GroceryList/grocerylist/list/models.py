c17e137099f3177d1d1d4c673fd56230519479e0
Cole Nation <52674619+Nation36@users.noreply.github.com>
2019-09-09 11:05:40 -0700

    .

--- code/cole/Django/GroceryList/grocerylist/list/models.py
+++ code/cole/Django/GroceryList/grocerylist/list/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 class GroceryItem(models.Model):
     itemText = models.CharField(max_length=200)
     itemCount = models.IntegerField()
     createdDate = models.DateTimeField()
     completedDate = models.DateTimeField(default = '')
-    completed = False
+    completed = models.BooleanField(default = False)
 
     def __str__(self):
         return self.itemText
+
+    def get_absolute_url(self):
+        return reverse('GroceryList:list')
