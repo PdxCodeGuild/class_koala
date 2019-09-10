404787d32ecec078d5d88f103ab299fbc2b4acad
exxon <suarez.exxon@gmail.com>
2019-09-09 20:24:54 -0700

    url shortener

--- code/exxons/Django/url shortener/short/models.py
+++ code/exxons/Django/url shortener/short/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.utils.text import slugify 
+
 
 class URL(models.Model):
     url_text = models.TextField()
-    pub_date = models.DateTimeField("date published")
-    short_code = models.SlugField(max_length=6)
+    pub_date = models.DateTimeField("pub_date")
+    short_url = models.CharField(max_length=6)
 
     def __str__(self):
         return self.url_text
--- code/exxons/Django/url shortener/short/views.py
+++ code/exxons/Django/url shortener/short/views.py
@@ -1,24 +1,46 @@
 from django.shortcuts import render
 from .models import URL
-from django.http import HttpResponse
+from django.urls  import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
 
-import random, string
+import random
+import string
+
+
+def code_gen():
+
+    chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
+    size = 6
+    return ''.join(random.choice(chars) for num in range(size))
+
+
+
 
 
 
 def index(request):
 
-    long_urls = URL.objects.order_by('-pub_date')[:5]
-    context = {'long_urls': long_urls}
+    long_urls = URL.objects.all()
+    context = {'long_urls':long_urls}
+    
     return render(request, 'short/index.html', context)
+
+
+
 
 def coder(request):
-    short_code = ""
-    letter_choices= ['b', 'c', 'd', 'e', 'f']
-    choices = (string.ascii_lowercase + string.punctuation)
-    short_code = short_code + (random.choice (choices)+ (random.choice (letter_choices)))
-    context = {'short_code': short_code}
+    new_code = code_gen()
+    URL.objects.create(pub_date=timezone.now(), url_text=request.POST['url_name'], new_code=new_code)
 
-    return render(request, 'short/index.html', context)
+    return HttpResponseRedirect(reverse('short:index'))
+
+
+def success(request,short_url):
+    
+    user_url = get_object_or_404(URL, url_text=short_url)
+    new_url = user_url.url_text
+    new_url.save()
+    return HttpResponseRedirect(new_url)
+
