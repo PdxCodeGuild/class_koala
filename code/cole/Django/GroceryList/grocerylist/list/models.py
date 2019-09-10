3e6034139c05b9d86380f1cee97ed2e1711d4a57
Cole Nation <52674619+Nation36@users.noreply.github.com>
2019-09-10 10:46:08 -0700

    .

--- code/cole/Django/GroceryList/grocerylist/list/models.py
+++ code/cole/Django/GroceryList/grocerylist/list/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 class GroceryItem(models.Model):
     itemText = models.CharField(max_length=200)
     itemCount = models.IntegerField()
-    createdDate = models.DateTimeField()
-    completedDate = models.DateTimeField(default = '')
+    createdDate = models.DateTimeField(default = timezone.now())
+    completedDate = models.DateTimeField(default = timezone.now())
     completed = models.BooleanField(default = False)
 
     def __str__(self):
