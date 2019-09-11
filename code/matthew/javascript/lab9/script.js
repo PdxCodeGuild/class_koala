99d0b0bc17579ac20969692827c6b3255ec6ea0e
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-11 15:52:41 -0700

    Completed Lab 9 - Version 1 / beginning Version 2

---
+++ code/matthew/javascript/lab9/script.js
@@ -0,0 +1,23 @@
+let quoteButton = document.getElementById("quote-button");
+let target = document.getElementById("target");
+
+quoteButton.addEventListener("click", function(e) {
+	axios({
+		method: "get",
+		url: "https://favqs.com/api/qotd",
+		headers: {
+			Authorization: "Token token = '813c6ce13e128db13774ca6487ddb3ac'"
+		}
+	})
+	.then(function(response) {
+		console.log(response);
+		let resultHTML = `
+			<p class="mt-4">${response.data.quote.body}</p>
+			<p><a href=${response.data.quote.url}>${response.data.quote.author}</a></p>
+		`;
+		target.innerHTML = resultHTML;
+	})
+	.catch(function(error) {
+		console.log(error);
+	});
+});
