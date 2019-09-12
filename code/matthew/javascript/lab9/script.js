ee49c5f520b255cfa6910db52d1b4b2b544682cd
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-12 15:58:25 -0700

    now enables pagination / need to add user input for custom filter

--- code/matthew/javascript/lab9/script.js
+++ code/matthew/javascript/lab9/script.js
@@ -1,13 +1,21 @@
+///////////// GLOBAL VARIABLES //////////////
+
+let page = 1;
+
 let quoteButton = document.getElementById("quote-button");
 let target = document.getElementById("target");
+let prevButton = document.getElementById("prevButton");
+let nextButton = document.getElementById("nextButton");
 
-quoteButton.addEventListener("click", function(e) {
+///////////// FUNCTIONS //////////////
+
+function getQuotes() {
 	axios({
 		method: "get",
 		baseURL: "https://favqs.com/api/quotes/",
 		params: {
-			type: "author",
-			filter: "Mark+Twain",
+			page: page,
+			filter: "peace",
 		},
 		headers: {
 			Authorization: 'Token token=""'
@@ -16,30 +24,53 @@ quoteButton.addEventListener("click", function(e) {
 	.then(function(response) {
 		console.log(response);
 		let quotes = response.data.quotes;
-		for(let i in quotes) {
-			let resultHTML = `
+		target.innerHTML = "";
+		for(let quote of quotes) {			
+			let currentQuotes = `
 			<div class="mt-4">
-				<p>${quotes[i].body}</p>
-				<p><a href=${quotes[i].url}>${quotes[i].author}</a></p>
+				<p>${quote.body}</p>
+				<p><a href=${quote.url}>${quote.author}</a></p>
 			</div>
 			`;
-			target.innerHTML += resultHTML;
+			target.innerHTML += currentQuotes;
 		}
-
-		// ANOTHER OPTION THAT WORKED, BUT I UNDERSTAND LESS =P
-		// let quotes = response.data.quotes;
-		// quotes.forEach(obj => {
-		// 	let resultHTML = `
-		// 	<div class="mt-4">
-		// 		<p>${obj.body}</p>
-		// 		<p><a href=${obj.url}>${obj.author}</a></p>
-		// 	</div>
-		// 	`;
-		// 	target.innerHTML += resultHTML;
-		// });
-
 	})
 	.catch(function(error) {
 		console.log(error);
-	});
-});
+	})
+}
+
+function next() {
+	page += 1;
+	if (page > 1) {
+		prevButton.classList.remove("d-none");
+	}
+}
+
+function previous() {
+	if (page > 1) {
+		page -= 1;
+	}
+	if (page === 1) {
+		prevButton.classList.add("d-none");
+	}
+}
+
+///////////// EVENT LISTENERS //////////////
+
+quoteButton.addEventListener("click", function() {
+	getQuotes();
+	nextButton.classList.remove("d-none");
+})
+
+nextButton.addEventListener("click", function(e) {
+	console.log(e);
+	next();
+	getQuotes();
+})
+
+prevButton.addEventListener("click", function(e) {
+	console.log(e);
+	previous();
+	getQuotes();
+})
