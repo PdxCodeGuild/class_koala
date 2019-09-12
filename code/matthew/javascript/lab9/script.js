3bb1c11ac2df4b996980e7adbe9f96b17387f364
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-11 19:57:01 -0700

    managed to list full page of filtered quotes on screen

--- code/matthew/javascript/lab9/script.js
+++ code/matthew/javascript/lab9/script.js
@@ -4,18 +4,40 @@ let target = document.getElementById("target");
 quoteButton.addEventListener("click", function(e) {
 	axios({
 		method: "get",
-		url: "https://favqs.com/api/qotd",
-		// headers: {
-		// 	Authorization: "Token token = ''"
-		// }
+		baseURL: "https://favqs.com/api/quotes/",
+		params: {
+			type: "author",
+			filter: "Mark+Twain",
+		},
+		headers: {
+			Authorization: 'Token token=""'
+		}
 	})
 	.then(function(response) {
 		console.log(response);
-		let resultHTML = `
-			<p class="mt-4">${response.data.quote.body}</p>
-			<p><a href=${response.data.quote.url}>${response.data.quote.author}</a></p>
-		`;
-		target.innerHTML = resultHTML;
+		let quotes = response.data.quotes;
+		for(let i in quotes) {
+			let resultHTML = `
+			<div class="mt-4">
+				<p>${quotes[i].body}</p>
+				<p><a href=${quotes[i].url}>${quotes[i].author}</a></p>
+			</div>
+			`;
+			target.innerHTML += resultHTML;
+		}
+
+		// ANOTHER OPTION THAT WORKED, BUT I UNDERSTAND LESS =P
+		// let quotes = response.data.quotes;
+		// quotes.forEach(obj => {
+		// 	let resultHTML = `
+		// 	<div class="mt-4">
+		// 		<p>${obj.body}</p>
+		// 		<p><a href=${obj.url}>${obj.author}</a></p>
+		// 	</div>
+		// 	`;
+		// 	target.innerHTML += resultHTML;
+		// });
+
 	})
 	.catch(function(error) {
 		console.log(error);
