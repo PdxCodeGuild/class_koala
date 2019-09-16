db4a4b42d123a3b7457a68f9ba7beb5f0f574a40
exxon <suarez.exxon@gmail.com>
2019-09-16 16:30:08 -0700

    started todo Vue

---
+++ code/exxons/JavaScript/lab_10/todos/Js/site.js
@@ -0,0 +1,33 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello world!',
+        todos:[
+
+        ],
+        newTodo: {
+            text: '',
+            isCompleted: false
+        },
+
+    
+},
+
+            
+            
+            
+
+methods: {
+    addTodo: function() {
+        this.todos.push({text: this.newTodo.text, isComplete: this.newTodo.isComplete})
+    },
+
+    eraseTodo: function(index){
+        this.todos.splice(index,1);
+    },
+
+    completeTask: function(todos){
+        todos.isCompleted = !todos.isCompleted;
+    }
+}
+});
