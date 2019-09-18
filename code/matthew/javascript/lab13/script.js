88008fdbd2e4dab9d30744c66c1097c6d8e192a3
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-18 14:39:30 -0700

    completed Lab 13

--- code/matthew/javascript/lab13/script.js
+++ code/matthew/javascript/lab13/script.js
@@ -1,3 +1,51 @@
+///////////// VUE COMPONENTS //////////////
+
+Vue.component('prev-button', {
+	props: [],
+	template:
+		`<li id="prevbutton" class="disabled" @click="pagePrev">
+			<a href="#!">
+			<i class="material-icons">chevron_left</i>
+			</a>
+			</i>
+		</li>`,
+	methods: {
+		pagePrev: function () {
+			if (this.$parent.currentPage === 2) {
+				prevbutton.classList.add("disabled");
+				};
+			if (this.$parent.currentPage === 1) {
+				prevbutton.classList.remove("waves-effect");
+				};
+			if (this.$parent.currentPage > 1) {
+				this.$parent.currentPage--;
+				this.$parent.getQuotes();
+				};
+			},
+	}
+})
+
+Vue.component('next-button', {
+	props: [],
+	template:
+		`<li id="nextbutton" class="waves-effect" @click="pageNext">
+			<a href="#!">
+			<i class="material-icons">chevron_right</i>
+			</a>
+			</i>
+		</li>`,
+	methods: {
+			pageNext: function() {
+				this.$parent.currentPage++;
+				this.$parent.getQuotes();
+				prevbutton.classList.remove("disabled");
+				prevbutton.classList.add("waves-effect")
+			},
+	}
+})
+
+
+
 ///////////// ROOT VUE //////////////
 
 new Vue({
@@ -7,6 +55,9 @@ new Vue({
 			searchValue: null,
 			typeValue: null,
 			quoteResults: [],
+			currentPage: 1,
+			perPage: 5,
+			pages: [],
 		}
 	},
 	methods: {
@@ -15,7 +66,7 @@ new Vue({
 				method: "get",
 				baseURL: "https://favqs.com/api/quotes/",
 				params: {
-					page: 1,
+					page: this.currentPage,
 					filter: this.searchValue,
 					type: this.typeValue,
 				},
@@ -30,54 +81,18 @@ new Vue({
 			.catch(function(error) {
 				console.log(error);
 			})
-		}
+		},
 	},
 	mounted() {
 		this.getQuotes();
 	}
 })
 
+
+
+///////////// MATERIALIZE //////////////
+
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('select');
 	var instances = M.FormSelect.init(elems);
 });
-
-
-
-
-///////////// GLOBAL VARIABLES //////////////
-
-// let page = 1;
-
-// let quoteButton = document.getElementById("quote-button");
-// let target = document.getElementById("target");
-// let prevButton = document.getElementById("prevButton");
-// let nextButton = document.getElementById("nextButton");
-// let userInput = document.getElementById("user-input");
-
-///////////// FUNCTIONS //////////////
-
-
-// function next() {
-// 	page += 1;
-// 	if (page > 1) {
-// 		prevButton.classList.remove("d-none");
-// 	}
-// }
-
-// function previous() {
-// 	if (page > 1) {
-// 		page -= 1;
-// 	}
-// 	if (page === 1) {
-// 		prevButton.classList.add("d-none");
-// 	}
-// }
-
-// function keyPress(e) {
-// 	if (e.which === 13) {
-// 	  getQuotes();
-// 	  nextButton.classList.remove("d-none");
-// 	  e.preventDefault();
-// 	}
-//   }
