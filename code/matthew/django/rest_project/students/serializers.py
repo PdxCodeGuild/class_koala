ae007c048a232b5c6ba9df00744c2f07cbc87453
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-20 15:24:01 -0700

    completed Lab 6 - Part 2

--- code/matthew/django/rest_project/students/serializers.py
+++ code/matthew/django/rest_project/students/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Student
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Student
-        fields = ['id', 'first_name', 'last_name', 'cohort', 'favorite_topic', 'favorite_teacher', 'capstone']
+	class Meta:
+		model = Student
+		fields = ['id', 'first_name', 'last_name', 'cohort', 'favorite_topic', 'favorite_teacher', 'capstone']
+		read_only_fields = ['id']
