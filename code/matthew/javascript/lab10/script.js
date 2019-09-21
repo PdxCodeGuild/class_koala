7aa93d067bdf89977e0c2d791d76e47360ab70b8
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-20 18:06:27 -0700

    completed Lab 10 / added search feature and styled card layout

--- code/matthew/javascript/lab10/script.js
+++ code/matthew/javascript/lab10/script.js
@@ -4,34 +4,37 @@ new Vue({
 	el: '#app',
 	data () {
 		return {
-			restaurants: null
+			restaurants: null,
+			searchValue: null,
 		}
 	},
 	methods: {
+		listCarts: function() {
+			axios({
+				method: "get",
+				baseURL: "https://developers.zomato.com/api/v2.1/",
+				url: "search",
+				headers: {
+					"user-key": "",
+				},
+				params: {
+					"entity_id": 286,
+					"entity_type": "city",
+					"q": this.searchValue,
+					"establishment_type": 81,
+					"count": 20,
+				}
+			})
+			.then((response) => {
+				console.log(response.data.restaurants);
+				this.restaurants = response.data.restaurants;
+			})
+			.catch(function(error) {
+				console.log(error);
+			})
+		}
 	},
-	mounted: function() {
-		axios({
-			method: "get",
-			baseURL: "https://developers.zomato.com/api/v2.1/",
-			url: "search",
-			headers: {
-				"user-key": "",
-			},
-			params: {
-				"entity_id": 286,
-				"entity_type": "city",
-				// "q": searchValue,
-				"establishment_type": 81,
-				"count": 5,
-			}
-		})
-		.then((response) => {
-			console.log(response);
-			console.log(response.data.restaurants[0].restaurant.thumb)
-			this.restaurants = response.data.restaurants;
-		})
-		.catch(function(error) {
-			console.log(error);
-		})
+	mounted() {
+		this.listCarts()
 	}
 })
