c7a2b067604726dadf27024cfe6c7a8a46aad6f9
Matthew J. Hughes <mjhughes707@gmail.com>
2019-09-20 20:38:07 -0700

    removed foodcartcity folder / will build in personal repo

--- code/matthew/capstone/foodcartcity/carts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "carts"
-urlpatterns = [
-    path("", views.CartListView.as_view(), name="home"),
-]
--- code/matthew/capstone/foodcartcity/carts/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView
-from .models import Cart, Cuisine
-
-class CartListView(ListView):
-    template_name = "home.html"
-
-    def get_queryset(self):
-        return Cart.objects.all
-    
