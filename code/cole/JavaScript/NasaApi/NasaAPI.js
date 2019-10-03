7ad3d1ce23c9144d0571f77dc7aaed3a8dbab264
Cole Nation <52674619+Nation36@users.noreply.github.com>
2019-10-03 11:46:47 -0700

    finished any api lab

--- code/cole/JavaScript/NasaApi/NasaAPI.js
+++ code/cole/JavaScript/NasaApi/NasaAPI.js
@@ -1,1 +1,35 @@
-GET "https://api.nasa.gov/insight_weather/?api_key=vD9wfXQzr8TbKNrbHrjUhblIduh25jVI6Q2xFxFN&feedtype=json&ver=1.0"
+
+new Vue({
+  el: '#app',
+  data (){
+    return {
+      data: null,
+      chosenDate: null,
+      dateData: null,
+      dateTempHigh: null,
+      dateTempLow: null,
+      dateWindAvg: null,
+      errorMessage: ''
+    }
+  },
+  methods: {
+    showData: function() {
+      if (this.chosenDate in this.data.data) {
+        this.errorMessage = '';
+        this.dateData = this.data.data[this.chosenDate];
+        this.dateTempHigh = `${this.dateData.AT.mx}` + '°C';
+        this.dateTempLow = `${this.dateData.AT.mn}` + '°C';
+        this.dateWindAvg = `${this.dateData.HWS.av}` + ' meters per second';
+      }
+      else {
+        this.errorMessage = 'Please enter a number between 296 and 302';
+      }
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.nasa.gov/insight_weather/?api_key=Lt3BvyhEO0U5a4eiPDuqi5L2pXsHCn77nDe975fP&feedtype=json&ver=1.0')
+      .then(response => (this.data = response))
+  }
+
+})
