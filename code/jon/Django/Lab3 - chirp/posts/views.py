e2b97890a5bd1df8eed7c62cd38ad2ad3c73d9b6
jonbascos <jon@jonbascos.com>
2019-10-07 09:55:31 -0700

    trying to finish up labs.  Working on Chirp lab

--- code/jon/Django/Lab3 - chirp/posts/views.py
+++ code/jon/Django/Lab3 - chirp/posts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-# Create your views here.
+from .models import Posts
+
+class PostsListView(ListView):
+    model = Posts
+    template_name = 'home.html'
+
+    def get_queryset(self):
+        return Posts.objects.order_by('-created')
+
+class PostsDetailView(DetailView):
+    model = Posts
+    template_name = "post_detail.html"
+
+class PostsCreateView(LoginRequiredMixin, CreateView):
+    model = Posts
+    template_name: "post_new.html"
+    fields = ["body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
--- code/jon/JavaScript/Lab 13 - FavQs in Vue/js/main.js
+++ code/jon/JavaScript/Lab 13 - FavQs in Vue/js/main.js
@@ -10,20 +10,30 @@ new Vue({
         search: function () {
             axios({
                 method: 'post',
-                baseURL: 'https://favqs.com/api/'
+                baseURL: 'https://favqs.com/api/',
                 url: 'quotes',
-                header: {
-                    Authorization: 'Token token=""'
-                }
-
-            }),
+                params: {
+                    filter: userInput,
+                    type: 'author',
+                    pageNumber: page,
+                },
+                headers: {
+                    Authorization: 'Token token="3349554fa3d88cf6a1eaa9f75482ac37"'
+                },
+            })
             .then(function(response) {
-                console.log(response)
+                console.log(response.data)
+                let quotesList = response.data.quotes;
+                quote.innerHTML = `<h1>Quotes by ${specificAuthor.value}`;
+                for (let i = 0; i < quotesList.length; i++) {
+                    let resultsHTML = `<p>
+                    <li>${quotesList[i].body}</li>
+                    </p>`
+    
+                    quote.innerHTML += resultsHTML;
+                }
             })
         }
-    },
-    mounted: function () {
-        this.search();
     },
 
     computed: {
