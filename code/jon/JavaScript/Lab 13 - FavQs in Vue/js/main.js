1a86ddbdc89f01226689f99045fc3fb6151d8419
jonbascos <jon@jonbascos.com>
2019-10-11 14:42:27 -0700

    finished FavQ Vue

--- code/jon/JavaScript/Lab 13 - FavQs in Vue/js/main.js
+++ code/jon/JavaScript/Lab 13 - FavQs in Vue/js/main.js
@@ -1,43 +1,68 @@
+Vue.component('previouspage', {
+    methods: {
+        previouspage: function () {
+
+            if (this.$parent.page === 2) {
+                previousbutton.style.display = 'inline';
+            } else if (this.$parent.page === 1) {
+                previousbutton.style.display = 'none';
+            } else if (this.$parent.page > 1) {
+                this.$parent.page--;
+                this.$parent.search();
+            };
+        }
+    },
+    template: `
+            <button id="previousbutton" @click='previouspage'>Previous</button>
+    `
+})
+
+Vue.component('nextpage', {
+
+    methods: {
+        nextpage: function () {
+            this.$parent.page++;
+            this.$parent.search();
+        }
+    },
+    template: `
+            <button id="nextbutton" @click='nextpage'>Next</button>
+    `
+})
+
+
 new Vue({
     el: '#app',
     data: {
-        userInput: '',
+        userInput: null,
         lastPage: '',
         page: 1,
-        quotes: '',
+        quotes: [],
+        searchby: null,
     },
     methods: {
         search: function () {
             axios({
-                method: 'post',
-                baseURL: 'https://favqs.com/api/',
-                url: 'quotes',
-                params: {
-                    filter: userInput,
-                    type: 'author',
-                    pageNumber: page,
-                },
-                headers: {
-                    Authorization: 'Token token="3349554fa3d88cf6a1eaa9f75482ac37"'
-                },
-            })
-            .then(function(response) {
-                console.log(response.data)
-                let quotesList = response.data.quotes;
-                quote.innerHTML = `<h1>Quotes by ${specificAuthor.value}`;
-                for (let i = 0; i < quotesList.length; i++) {
-                    let resultsHTML = `<p>
-                    <li>${quotesList[i].body}</li>
-                    </p>`
-    
-                    quote.innerHTML += resultsHTML;
-                }
-            })
-        }
+                    method: 'get',
+                    baseURL: 'https://favqs.com/api/',
+                    url: 'quotes',
+                    params: {
+                        filter: this.userInput,
+                        type: this.searchby,
+                        pageNumber: this.page,
+                    },
+                    headers: {
+                        Authorization: 'Token token="3349554fa3d88cf6a1eaa9f75482ac37"'
+                    },
+                })
+                .then((response) => {
+                    this.quotes = response.data.quotes;
+                    console.log(this.quotes);
+                })
+        },
     },
-
-    computed: {
-
+    mounted: function () {
+        this.search();
     }
 
 })
